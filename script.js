7e4974ed6e588b0153026dcd5ca48984390d2ab7
tawakuliKH <tawakuli456@gmail.com>
2023-02-07 22:51:41 +0430

    Update script.js file for interecting with mobile menus

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+let wrapper = document.querySelector('.wrapper');
 let btn = document.querySelector(".vector");
-let mobileMenu = document.querySelector(".navbar-mobile");
+let mobileMenu = document.querySelector(".nav-mobile");
+let humberger = document.querySelector('.vector')
 let closeIcon = document.querySelector(".cross-btn");
+let menu = document.querySelectorAll('.menu-mobile');
 
+
+let header =document.querySelector('.head');
 function toggleMenu() {
-    if (mobileMenu.style.display == "none") {
-        mobileMenu.style.display = "block";
-    } else {
-        mobileMenu.style.display = "none"
-    }
+  if (mobileMenu.style.display === 'none') {
+    mobileMenu.style.display = 'block';
+    humberger.style.display = 'none';
+    header.style.position='static'
+  } else {
+    mobileMenu.style.display = 'none';
+  }
 }
 
-btn.addEventListener("click", toggleMenu)
+function closeMenu() {
+  if (mobileMenu.style.display === 'block') {
+    mobileMenu.style.display = 'none';
+    humberger.style.display = 'block';
+    header.style.position='fixed';
+    
+  }
+}
+
+function menuClick() {
+  if (mobileMenu.style.display === 'block') {
+    mobileMenu.style.display = 'none';
+    humberger.style.display = 'block';
+    header.style.position='fixed';
+
+  }
+}
+
+btn.addEventListener("click", toggleMenu);
+closeIcon.addEventListener('click', closeMenu);
+
+let arr = Array.from(menu);
+for (let i = 0; i < arr.length; i++) {
+  arr[i].addEventListener('click', menuClick);
+}
